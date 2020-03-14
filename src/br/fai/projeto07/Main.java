19cc77a8250bb3b2db0e2fc0d2ee984ece02ce19
Ana Terezinha <aterezinha.19@hotmail.com>
2020-03-14 01:21:10 -0300

    primeiro commit

---
+++ src/br/fai/projeto07/Main.java
@@ -0,0 +1,38 @@
+package br.fai.projeto07;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Main app = new Main();
+		app.start();
+		
+		
+	}
+
+	private void start() {
+		
+		operacoes(5, 5);
+		operacoes(10.27 , 10.42);
+		operacoes("44 anos", "Meu pai tem ");
+			}
+	private void operacoes(int num, int num1) {
+		
+		num= num+num1;
+		System.out.println("O resultado da operação e: "+num);
+	}
+	
+    private void operacoes(double num, double num1) {
+		
+		num=num+num1;
+		 
+		System.out.println("O resultado da operação e: "+num);
+			}
+	private void operacoes(String num, String texto) {
+		
+		String result= texto.concat(num);
+		
+		System.out.println("O resultado da operação e: "+result);
+	}
+		
+	
+}
